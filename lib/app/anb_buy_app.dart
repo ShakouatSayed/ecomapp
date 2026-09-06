ed919650c7868380bf09f4a33d5e0a48527b92d6
Shakouat Hossen <sayedckb61@gmail.com>
2026-09-06 07:16:30 +0600

    locale_provider

--- lib/app/anb_buy_app.dart
+++ lib/app/anb_buy_app.dart
@@ -1,6 +1,7 @@
 import 'package:ecomapp/app/app_routes.dart';
 import 'package:ecomapp/app/app_theme.dart';
 import 'package:ecomapp/app/extensions/localization_extension.dart';
+import 'package:ecomapp/app/providers/local_provider.dart';
 import 'package:ecomapp/app/providers/theme_provider.dart';
 import 'package:ecomapp/features/auth/presentation/screens/splash_screen.dart';
 import 'package:ecomapp/l10n/app_localizations.dart';
@@ -16,7 +17,7 @@ class ANBBuyApp extends StatefulWidget {
 }
 
 class _ANBBuyAppState extends State<ANBBuyApp> {
-  //final LocaleProvider _localeProvider = LocaleProvider();
+  final LocaleProvider _localeProvider = LocaleProvider();
 
   final ThemeProvider _themeProvider = ThemeProvider();
 
@@ -24,29 +25,36 @@ class _ANBBuyAppState extends State<ANBBuyApp> {
   void initState() {
     super.initState();
     _themeProvider.init();
-    //_localeProvider.init();
+    _localeProvider.init();
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider.value(value: _localeProvider)],
-      child: Consumer<LocaleProvider>(
+      providers: [
+        ChangeNotifierProvider.value(value: _localeProvider),
+        ChangeNotifierProvider.value(value: _themeProvider),
+      ],
+      child: Consumer<ThemeProvider>(
         builder: (context, _, _) {
-          return MaterialApp(
-            title: "ANB Bay",
-            home: SplashScreen(),
-            initialRoute: SplashScreen.name,
-            localizationsDelegates: [
-              AppLocalizations.delegate,
-              ...GlobalMaterialLocalizations.delegates,
-            ],
-            onGenerateRoute: AppRoutes.onGeneateRoute,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: ThemeMode.light,
-            supportedLocales: _localeProvider.supportedLocales,
-            locale: _localeProvider.currentLocale,
+          return Consumer<LocaleProvider>(
+            builder: (context, _, _) {
+              return MaterialApp(
+                title: "ANB Bay",
+                home: SplashScreen(),
+                initialRoute: SplashScreen.name,
+                localizationsDelegates: [
+                  AppLocalizations.delegate,
+                  ...GlobalMaterialLocalizations.delegates,
+                ],
+                onGenerateRoute: AppRoutes.onGeneateRoute,
+                theme: AppTheme.lightTheme,
+                darkTheme: AppTheme.darkTheme,
+                themeMode: _themeProvider.currentThemeMode,
+                supportedLocales: _localeProvider.supportedLocales,
+                locale: _localeProvider.currentLocale,
+              );
+            },
           );
         },
       ),
